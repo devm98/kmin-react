44c96713c3c278975f85e942d19c96ca4be53e39
KhaMinh <khaminhbui@gmail.com>
2019-04-14 16:16:05 +0700

    auto increase

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,30 @@ import React from 'react'
 
 import Counter from './Counter'
 
-function App() {
-  return <Counter />
+class App extends React.Component{
+  state = {showCounter: true}
+
+  handleToggleCounter = () =>{
+    this.setState({showCounter: !this.state.showCounter})
+  }
+  render() {
+    return (
+      <div>
+        {this.state.showCounter ? <Counter /> : null}
+        <button style={{
+          background: 'red',
+          borderRadius: 5,
+          padding: '10px',
+          color: 'yellow',
+          fontWeight: 700,
+          border: '1px solid transparent',
+          margin: '10px',
+          outline: 'none',
+          cursor: 'pointer',
+        }} onClick={this.handleToggleCounter}>{this.state.showCounter ? 'Hide' : 'Show'}</button>
+      </div>
+    )
+  }
 }
 
 export default App;
--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,64 +1,97 @@
-import React from 'react' 
+import React from 'react'
 
 
-// var styleBtn = {
-    //     border = '1px solid transperant',   
-    // }
-    class Counter extends React.Component{
-        state={ value : 0, delta: 0,} 
+class Counter extends React.Component {
+  state = { value: 0, delta: 0, autoIncreaseOn : false }
+  interval = null
+  // tang = () =>{
+  //     const currentValue = this.state.value
+  //     this.setState({value: currentValue + 1})
+  // }
+  // giam = () =>{
+  //     const currentValue = this.state.value
+  //     this.setState({value: currentValue - 1})
+  // }
+  reset = () => {
+    // const currentValue = this.state.value
+    this.setState({ value: 0 })
+  }
 
-        // tang = () =>{
-        //     const currentValue = this.state.value
-        //     this.setState({value: currentValue + 1})
-        // }
-        // giam = () =>{
-        //     const currentValue = this.state.value
-        //     this.setState({value: currentValue - 1})
-        // }
-        reset = () => {
-            const currentValue = this.state.value
-            this.setState({value: 0})
-        } 
-
-        
-      handleValueChange = (delta) => {
-        const currentValue = this.state.value
-        this.setState({ value: currentValue + delta })
+  handleValueChange = delta => {
+    const currentValue = this.state.value
+    this.setState({ value: currentValue + delta })
+  }
+  handleDeltaChange = delta1 => {
+    const currentDelta = this.state.delta
+    this.setState({ delta: currentDelta + delta1 })
+  }
+  autoIncrease = () =>{
+    this.setState({ autoIncreaseOn: !this.state.autoIncreaseOn}, ()=>{
+      if(this.state.autoIncreaseOn){
+        this.interval = setInterval(()=>{
+          this.upDateValue()
+        }, 500)
+      } else {
+        clearInterval(this.interval)
       }
-      handleDeltaChange = (delta1) => {
-        const currentDelta = this.state.delta
-        this.setState({ delta: currentDelta + delta1 })
-      }
+    })
+  }
+  upDateValue = () => {
+    const currentDelta = this.state.delta
+    const currentValue = this.state.value
+    this.setState({ value: currentDelta + currentValue })
+  }
+componentDidMount = () =>{
+  console.log("componentDiMount")
+}
 
-      upDateValue = () =>{
-        const currentDelta = this.state.delta
-        const currentValue = this.state.value
-        this.setState({value : currentDelta + currentValue})
-      }
-      render() {
-        const value = this.state.value
-        return (<div>
-          <div >Current value: <span style={{ color: changeColor(value) }}>{value}</span></div>
+  render() {
+    console.log('componentRender')
+    const value = this.state.value
+    return (
+      <div style={modelBody}>
+        <div style={{color: 'white'}}>
+          Current value:{' '}
+          <span style={{ color: changeColor(value) }}>{value}</span>
+        </div>
+        <div>
           <div>
-            <div>
-              <button onClick={this.reset}>Reset</button>
-            </div>
-            <button onClick={() => this.handleValueChange(1)}>Up</button>
-            <button onClick={() => this.handleValueChange(-1)}>Down</button>
-            <div>Delta: {this.state.delta} </div>
-            <button onClick={() => this.handleDeltaChange(1)}>Up</button>
-            <button onClick={() => this.handleDeltaChange(-1)}>Down</button>
+            <button style={modelBtn} onClick={this.reset}>Reset</button>
           </div>
-          <button onClick={this.upDateValue}>Update</button>
-        </div>)
-      }
+          <button style={modelBtn} onClick={() => this.handleValueChange(1)}>Up</button>
+          <button style={modelBtn} onClick={() => this.handleValueChange(-1)}>Down</button>
+          <div style={{color: 'white'}}>Delta: {this.state.delta} </div>
+          <button style={modelBtn} onClick={() => this.handleDeltaChange(1)}>Up</button>
+          <button style={modelBtn} onClick={() => this.handleDeltaChange(-1)}>Down</button>
+        </div>
+        <button style={modelBtn} onClick={this.upDateValue}>Update</button>
+        <button style={modelBtn} onClick={this.autoIncrease}>
+            {this.state.autoIncreaseOn ? 'Stop' : 'Start'} Auto Increase
+          </button>
+      </div>
+    )
+  }
+  componentWillUnmount = () =>{
+    console.log('componentWill..')
+  }
+}
+  const modelBtn = {
+    background: 'blue',
+    borderRadius: 5,
+    padding: '10px',
+    color: 'white',
+    border: '1px solid transparent',
+    margin: '10px',
+    outline: 'none',
+    cursor: 'pointer',
+  }
+  const modelBody = {
+    background: 'black',
+  }
+const changeColor = value => {
+  if (value > 0) return 'green'
+  if (value < 0) return 'red'
+  return 'yellow'
 }
 
-
-const changeColor = value =>{
-    if(value>0) return 'green'
-    if(value<0) return 'red'
-    return 'black'
-}
-
-export default Counter 
+export default Counter
